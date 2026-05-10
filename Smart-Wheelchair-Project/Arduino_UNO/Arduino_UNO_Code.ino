0185f979413fbce9568e0564e19596b0b26bfa50
Arjun Selvaraj <145590376+Arjuns20004@users.noreply.github.com>
2026-05-10 14:23:20 +0530

    Add files via upload

---
+++ Smart-Wheelchair-Project/Arduino_UNO/Arduino_UNO_Code.ino
@@ -0,0 +1,11 @@
+
+// Arduino UNO Smart Wheelchair Code
+// RFID + Voice + Eye Blink + GSM + Obstacle Detection
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Main control logic
+}
---
+++ Smart-Wheelchair-Project/ESP32_Controller/ESP32_Final_Code.ino
@@ -0,0 +1,11 @@
+
+// ESP32 Smart Wheelchair Controller Code
+// WiFi + Joystick + Touch + Rain Shelter
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // ESP32 control logic
+}
